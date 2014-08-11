507adde9742bc55acdef7ac6087e54bad846d5b9
Olivier Scherrer <pode.fr@gmail.com>
2014-08-10 20:37:14 -0400

    fun with ... rest params

--- arrow-function.js
+++ arrow-function.js
@@ -8,11 +8,11 @@ function Klass() {
 
     this.test = (p, q = 6) => p * q * this.value;
 
-    this.rest = (...args) => console.log(args);
+    this.rest = (first, ...otherParams) => console.log(otherParams);
 }
 
 var klass = new Klass();
 
 console.log(klass.test(3));
 
-console.log(klass.rest("test", "other test"));
+console.log(klass.rest("test", "other test", "last param"));
