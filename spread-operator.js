622bbc23626c87a678e7a7ce3a23477fa1393fb2
Olivier Scherrer <pode.fr@gmail.com>
2014-08-10 21:10:39 -0400

    more fun with spread operator

--- spread-operator.js
+++ spread-operator.js
@@ -9,3 +9,11 @@ console.log(test(...[0, 1, 2]));
 var array = [10, 20, 30];
 
 console.log(test(...array));
+
+
+var array1 = [0, 1, 5];
+var array2 = [2, 3, 4];
+
+array1.splice(2, 0, ...array2);
+
+console.log(array1);
