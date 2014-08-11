d78c856b23c1b5755cf6d57fb74aa3a7b58d369f
Olivier Scherrer <pode.fr@gmail.com>
2014-08-10 21:39:39 -0400

    fun with shorthand methods

--- shorthand.js
+++ shorthand.js
@@ -5,3 +5,11 @@
 var a = 1, b = 2, c = {a, b};
 
 console.log(c);
+
+var one = ({ val: 0,
+    plus: function () { this.val++; return this; },
+    minus: function (){ this.val--; return this; },
+    getVal: function() { return this.val; }
+}).plus().plus().minus().getVal();
+
+console.log(one);
