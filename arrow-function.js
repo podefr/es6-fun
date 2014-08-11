259dc0f69c2e16484007178f6a4f4d90f9a61b79
Olivier Scherrer <pode.fr@gmail.com>
2014-08-10 20:35:25 -0400

    test default param

--- arrow-function.js
+++ arrow-function.js
@@ -7,8 +7,12 @@ function Klass() {
     this.value = 2;
 
     this.test = (p, q = 6) => p * q * this.value;
+
+    this.rest = (...args) => console.log(args);
 }
 
 var klass = new Klass();
 
 console.log(klass.test(3));
+
+console.log(klass.rest("test", "other test"));
