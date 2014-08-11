adcd394769c006eb962036a7b401b1a897ef77ab
Olivier Scherrer <pode.fr@gmail.com>
2014-08-10 20:19:33 -0400

    test generators; yield; promise

---
+++ generators.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var fs = require("fs");
+
+function getFile(fileName) {
+    return new Promise(function (resolve, reject) {
+        fs.readFile(fileName, function (err, file) {
+            if (err) {
+                reject(err);
+            }
+            resolve(file);
+        });
+    });
+}
+
+function getFilesInDir() {
+    return new Promise(function (resolve, reject) {
+        fs.readdir(".", function (err, directory) {
+            if (err) {
+                console.log("mince")
+                reject(err);
+            }
+            resolve(directory);
+        });
+    });
+}
+
+function getMDFiles() {
+    getFilesInDir().then(function (files) {
+        files
+            .filter(isMDFile)
+            .forEach(getFile);
+    });
+}
+
+function isMDFile(file) {
+    return /\.md$/.test(file);
+}
+
+var mdFiles = getMDFiles();
